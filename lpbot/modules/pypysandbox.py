95a89a6d841c69218e1334adb06d2831a0158375
Nikola-K <nikolak@outlook.com>
2015-04-30 23:24:30 +0200

    Update to python3 subprocess, drop custom implementation

--- lpbot/modules/pypysandbox.py
+++ lpbot/modules/pypysandbox.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Popen, child process taken from
-# http://stackoverflow.com/questions/1191374/subprocess-with-timeout
 # PyPy code from: https://github.com/raylu/pbot/blob/master/commands.py
 
 
 
 import os
-import subprocess
-import time
-import select
-import errno
+from subprocess import Popen, PIPE, TimeoutExpired
+import signal
 
 from lpbot.module import commands, example
 
@@ -19,110 +15,25 @@ from lpbot.module import commands, example
 PATH = os.environ['PATH']
 
 
-class Popen(subprocess.Popen):
-    def communicate(self, input=None, timeout=None):
-        if timeout is None:
-            return subprocess.Popen.communicate(self, input)
-
-        if self.stdin:
-            # Flush stdio buffer, this might block if user
-            # has been writing to .stdin in an uncontrolled
-            # fashion.
-            self.stdin.flush()
-            if not input:
-                self.stdin.close()
-
-        read_set, write_set = [], []
-        stdout = stderr = None
-
-        if self.stdin and input:
-            write_set.append(self.stdin)
-        if self.stdout:
-            read_set.append(self.stdout)
-            stdout = []
-        if self.stderr:
-            read_set.append(self.stderr)
-            stderr = []
-
-        input_offset = 0
-        deadline = time.time() + timeout
-
-        while read_set or write_set:
-            try:
-                rlist, wlist, xlist = select.select(read_set, write_set, [], max(0, deadline - time.time()))
-            except select.error as ex:
-                if ex.args[0] == errno.EINTR:
-                    continue
-                raise
-
-            if not (rlist or wlist):
-                # Just break if timeout
-                # Since we do not close stdout/stderr/stdin, we can call
-                # communicate() several times reading data by smaller pieces.
-                break
-
-            if self.stdin in wlist:
-                chunk = input[input_offset:input_offset + subprocess._PIPE_BUF]
-                try:
-                    bytes_written = os.write(self.stdin.fileno(), chunk)
-                except OSError as ex:
-                    if ex.errno == errno.EPIPE:
-                        self.stdin.close()
-                        write_set.remove(self.stdin)
-                    else:
-                        raise
-                else:
-                    input_offset += bytes_written
-                    if input_offset >= len(input):
-                        self.stdin.close()
-                        write_set.remove(self.stdin)
-
-            # Read stdout / stderr by 1024 bytes
-            for fn, tgt in (
-                    (self.stdout, stdout),
-                    (self.stderr, stderr),
-            ):
-                if fn in rlist:
-                    data = os.read(fn.fileno(), 1024)
-                    if data == '':
-                        fn.close()
-                        read_set.remove(fn)
-                    tgt.append(data)
-
-        if stdout is not None:
-            stdout = ''.join(stdout)
-        if stderr is not None:
-            stderr = ''.join(stderr)
-
-        return (self.pid, stdout, stderr)
-
-
 def get_process_children(pid):
     p = Popen('ps --no-headers -o pid --ppid %d' % pid, shell=True,
-              stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+              stdout=PIPE, stderr=PIPE)
     stdout, stderr = p.communicate()
     return [int(p) for p in stdout.split()]
 
 
 def python(code):
     pypy = Popen(['pypy-sandbox'],
-                 stdin=subprocess.PIPE, stdout=subprocess.PIPE,
-                 stderr=subprocess.PIPE,
+                 stdin=PIPE, stdout=PIPE,
+                 stderr=PIPE,
                  env={'PATH': PATH},
                  universal_newlines=True,
                  preexec_fn=os.setpgrp)
-
-    start = time.time()
-    pid, stdout, stderr = pypy.communicate(code, timeout=5)
-
-    end = time.time()
-    # os.system("killall pypy-c-sandbox")
-
-    for process_pid in [pid] + get_process_children(pid):
-        os.system("kill {}".format(process_pid))
-
-    if end - start > 4:
-        return "Timed out"
+    try:
+        stdout, stderr = pypy.communicate(code, timeout=5)
+    except TimeoutExpired:
+        os.killpg(pypy.pid, signal.SIGKILL)
+        return "Timed out..."
 
     errlines = stderr.split('\n')
     if len(errlines) > 3:
