a93194e9af66ad7a6b2a04729c9f39aee1747c70
Nikola-K <nikolak@outlook.com>
2015-05-15 01:01:39 +0200

    Rewrite of the youtube module, v3 API support, livestream support, dropped channel info support

--- lpbot/modules/youtube.py
+++ lpbot/modules/youtube.py
@@ -1,195 +1,222 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2012, Dimitri Molenaars, Tyrope.nl.
-# Copyright © 2012-2014, Elad Alfassa, <elad@fedoraproject.org>
-# Copyright 2012, Edward Powell, embolalia.net
-# Copyright 2014, Nikola Kovacevic, <nikolak@outlook.com>
-# Licensed under the Eiffel Forum License 2.
+# Copyright 2015 Nikola Kovacevic <nikolak@outlook.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import re
 
 import requests
-import re
-from html.parser import HTMLParser
+import arrow
 
 from lpbot import tools
 from lpbot.module import rule, commands, example
 
 
-regex = re.compile('(youtube.com/watch\S*v=|youtu.be/)([\w-]+)')
+duration_regex = re.compile('PT(\d+H)?(\d+M)?(\d+S)?')
+BASE_URL = "https://www.googleapis.com/youtube/v3"
+
+
+def checkConfig(bot):
+    if not bot.config.has_option('youtube', 'api_key'):
+        return None, None
+    else:
+        return bot.config.youtube.api_key
+
+
+def configure(config):
+    """
+    | [youtube]| example | purpose |
+    | -------- | ------- | ------- |
+    | api_key | ASkdasfn3k259283askdhSAT5OADOAKjbh | Youtube v3 API key |
+    """
+    chunk = ''
+    if config.option('Configuring youtube module', False):
+        config.interactive_add('youtube', 'api_key', 'Youtube v3 API key', '')
+    return chunk
 
 
 def setup(bot):
     if not bot.memory.contains('url_callbacks'):
         bot.memory['url_callbacks'] = tools.lpbotMemory()
-    bot.memory['url_callbacks'][regex] = ytinfo
+    bot.memory['yt_key'] = checkConfig(bot)
 
 
-def shutdown(bot):
-    del bot.memory['url_callbacks'][regex]
+def get_video_info(api_key, video_id):
+    video_url = "/videos" \
+                "?part=contentDetails%2Cid%2CliveStreamingDetails%2Csnippet%2Cstatistics" \
+                "&id={VIDEO_ID}" \
+                "&key={API_KEY}".format(VIDEO_ID=video_id,
+                                        API_KEY=api_key)
 
+    endpoint = BASE_URL + video_url
 
-def ytget(bot, trigger, uri):
-    r = requests.get(uri)
-    result = r.json()
+    r = requests.get(endpoint)
+    if r.status_code != 200:
+        return None
+
+    values = {}
     try:
-        if 'feed' in result:
-            video_entry = result['feed']['entry'][0]
-        else:
-            video_entry = result['entry']
-    except KeyError:
-        return {'link': 'N/A'}  # Empty result
+        data = r.json()['items'][0]
+    except:
+        return None
 
-    vid_info = {}
-    try:
-        # The ID format is tag:youtube.com,2008:video:RYlCVwxoL_g
-        # So we need to split by : and take the last item
-        vid_id = video_entry['id']['$t'].split(':')
-        vid_id = vid_id[len(vid_id) - 1]  # last item is the actual ID
-        vid_info['link'] = 'http://youtu.be/' + vid_id
-    except KeyError:
-        vid_info['link'] = 'N/A'
+    values['id'] = data['id']
 
     try:
-        vid_info['title'] = video_entry['title']['$t']
+        is_live = data['contentDetails']['duration'] == "PT0S" and data.get('liveStreamingDetails')
     except KeyError:
-        vid_info['title'] = 'N/A'
+        is_live = False
 
-    # get youtube channel
+    values['livestream'] = is_live
+
+    values['title'] = data['snippet']['title']
     try:
-        vid_info['uploader'] = video_entry['author'][0]['name']['$t']
+        values['description'] = data['snipppet']['description']
     except KeyError:
-        vid_info['uploader'] = 'N/A'
+        values['description'] = "N/A"
 
-    # get upload time in format: yyyy-MM-ddThh:mm:ss.sssZ
     try:
-        upraw = video_entry['published']['$t']
-        #parse from current format to output format: DD/MM/yyyy, hh:mm
-        vid_info['uploaded'] = '%s/%s/%s, %s:%s' % (upraw[8:10], upraw[5:7],
-                                                    upraw[0:4], upraw[11:13],
-                                                    upraw[14:16])
+        values['uploader'] = data['snippet']['channelTitle']
     except KeyError:
-        vid_info['uploaded'] = 'N/A'
+        values['uploader'] = "N/A"
 
-    #get duration in seconds
+    if is_live:
+        values['duration'] = "LIVE"
+    else:
+        try:
+            duration = data['contentDetails']['duration']
+            d = re.match(duration_regex, duration)
+            d_values = [i for i in d.groups() if i is not None]
+            values['duration'] = " ".join(d_values). \
+                replace("H", "hr"). \
+                replace("M", "min"). \
+                replace("S", "sec")
+        except:
+            values['duration'] = "N/A"
+
+    values['uploaded'] = arrow.get(data['snippet']['publishedAt']).humanize()
     try:
-        duration = int(video_entry['media$group']['yt$duration']['seconds'])
-        #Detect liveshow + parse duration into proper time format.
-        if duration < 1:
-            vid_info['length'] = 'LIVE'
-        else:
-            hours = duration // (60 * 60)
-            minutes = duration // 60 - (hours * 60)
-            seconds = duration % 60
-            vid_info['length'] = ''
-            if hours:
-                vid_info['length'] = str(hours) + 'hours'
-                if minutes or seconds:
-                    vid_info['length'] = vid_info['length'] + ' '
-            if minutes:
-                vid_info['length'] = vid_info['length'] + str(minutes) + 'mins'
-                if seconds:
-                    vid_info['length'] = vid_info['length'] + ' '
-            if seconds:
-                vid_info['length'] = vid_info['length'] + str(seconds) + 'secs'
+        views = data['statistics']['viewCount']
+        values['views'] = str('{0:20,d}'.format(int(views))).lstrip(' ')
     except KeyError:
-        vid_info['length'] = 'N/A'
+        values['views'] = "N/A"
 
-    #get views
     try:
-        views = video_entry['yt$statistics']['viewCount']
-        vid_info['views'] = str('{0:20,d}'.format(int(views))).lstrip(' ')
+        likes = data['statistics']['likeCount']
+        values['likes'] = str('{0:20,d}'.format(int(likes))).lstrip(' ')
     except KeyError:
-        vid_info['views'] = 'N/A'
+        values['likes'] = "N/A"
 
-    #get comment count
     try:
-        comments = video_entry['gd$comments']['gd$feedLink']['countHint']
-        vid_info['comments'] = str('{0:20,d}'.format(int(comments))).lstrip(' ')
+        dislikes = data['statistics']['dislikeCount']
+        values['dislikes'] = str('{0:20,d}'.format(int(dislikes))).lstrip(' ')
     except KeyError:
-        vid_info['comments'] = 'N/A'
+        values['dislikes'] = "N/A"
 
-    #get likes & dislikes
     try:
-        likes = video_entry['yt$rating']['numLikes']
-        vid_info['likes'] = str('{0:20,d}'.format(int(likes))).lstrip(' ')
+        comments = data['statistics']['commentCount']
+        values['comments'] = str('{0:20,d}'.format(int(comments))).lstrip(' ')
     except KeyError:
-        vid_info['likes'] = 'N/A'
-    try:
-        dislikes = video_entry['yt$rating']['numDislikes']
-        vid_info['dislikes'] = str('{0:20,d}'.format(int(dislikes))).lstrip(' ')
-    except KeyError:
-        vid_info['dislikes'] = 'N/A'
-    return vid_info
+        values['comments'] = "N/A"
+
+    if is_live:
+        try:
+            values['streamStart'] = arrow.get(data['liveStreamingDetails']['actualStartTime']).humanize()
+        except:
+            values['streamStart'] = "N/A"
+
+        try:  # Not used currently
+            values['streamSchedule'] = arrow.get(data['liveStreamingDetails']['scheduledStartTime']).humanize()
+        except:
+            values['streamSchedule'] = "N/A"
+
+        try:
+            concurrentViewers = data['liveStreamingDetails']['concurrentViewers']
+            values['concurrentViewers'] = str('{0:20,d}'.format(int(concurrentViewers))).lstrip(' ')
+        except:
+            values['concurrentViewers'] = "N/A"
+
+    return values
 
 
-@commands('yt', 'youtube')
-@example('.yt how to be a nerdfighter FAQ')
-def ytsearch(bot, trigger):
-    """Search YouTube"""
-    # modified from ytinfo: Copyright 2010-2011, Michael Yanovich, yanovich.net, Kenneth Sham.
-    if not trigger.group(2):
+def get_search_result(api_key, query):
+    search_url = "/search?part=id&q={query}" \
+                 "&maxResults=1" \
+                 "&key={API_KEY}".format(query=query,
+                                         API_KEY=api_key)
+
+    endpoint = BASE_URL + search_url
+
+    r = requests.get(endpoint)
+
+    if r.status_code != 200:
         return
-    uri = 'https://gdata.youtube.com/feeds/api/videos?v=2&alt=json&max-results=1&q=' + trigger.group(2)
-    video_info = ytget(bot, trigger, uri)
 
-    if video_info is 'err':
+    try:
+        return r.json()['items'][0]['id']['videoId']
+    except:
         return
 
-    if video_info['link'] == 'N/A':
-        bot.say("Sorry, I couldn't find the video you are looking for")
-        return
-    message = ('[YT Search] Title: ' + video_info['title'] +
-               ' | Uploader: ' + video_info['uploader'] +
-               ' | Duration: ' + video_info['length'] +
-               ' | Uploaded: ' + video_info['uploaded'] +
-               ' | Views: ' + video_info['views'] +
-               ' | Link: ' + video_info['link'])
 
-    bot.say(HTMLParser().unescape(message))
+@commands('yotube', 'yt')
+@example('.yt example title')
+def youtube_search(bot, trigger):
+    query = trigger.group(2)
+    video_id = get_search_result(bot.memory['yt_key'], query)
+    if not video_id:
+        bot.say("[YT Search] Nothing found.")
+        return
+
+    values = get_video_info(bot.memory['yt_key'], video_id)
+    if not values:
+        return
+
+    message = "[YouTube Search] Title: {title} |  " \
+              "Uploader: {uploader} | " \
+              "Uploaded: {uploaded} | " \
+              "Duration: {duration} | " \
+              "Views: {views} | " \
+              "Likes: {likes} | " \
+              "Dislikes: {dislikes} | " \
+              "Link: https://youtu.be/{id}".format(**values)
+
+    bot.say(message)
 
 
 @rule('.*(youtube.com/watch\S*v=|youtu.be/)([\w-]+).*')
-def ytinfo(bot, trigger, found_match=None):
-    """
-    Get information about the latest video uploaded by the channel provided.
-    """
-    match = found_match or trigger
-    # Grab info from YT API
-    uri = 'https://gdata.youtube.com/feeds/api/videos/' + match.group(2) + '?v=2&alt=json'
-
-    video_info = ytget(bot, trigger, uri)
-    if video_info is 'err':
+def video_info(bot, trigger):
+    video_id = trigger.group(2)
+    values = get_video_info(bot.memory['yt_key'], video_id)
+    if not values:
         return
 
-    # combine variables and print
-    message = '[YouTube] Title: ' + video_info['title'] + \
-              ' | Uploader: ' + video_info['uploader'] + \
-              ' | Uploaded: ' + video_info['uploaded'] + \
-              ' | Duration: ' + video_info['length'] + \
-              ' | Views: ' + video_info['views'] + \
-              ' | Comments: ' + video_info['comments'] + \
-              ' | Likes: ' + video_info['likes'] + \
-              ' | Dislikes: ' + video_info['dislikes']
+    if values.get('livestream'):
+        message = "[YouTube Livestream] Title: {title} |  " \
+                  "Uploader: {uploader} | " \
+                  "Stream Started: {streamStart} | " \
+                  "Views: {views} | " \
+                  "Comments: {comments} | " \
+                  "Likes: {likes} | " \
+                  "Dislikes: {dislikes}".format(**values)
+    else:
+        message = "[YouTube] Title: {title} |  " \
+                  "Uploader: {uploader} | " \
+                  "Uploaded: {uploaded} | " \
+                  "Duration: {duration} | " \
+                  "Views: {views} | " \
+                  "Comments: {comments} | " \
+                  "Likes: {likes} | " \
+                  "Dislikes: {dislikes}".format(**values)
 
-    bot.say(HTMLParser().unescape(message))
-
-
-@commands('ytlast', 'ytnew', 'ytlatest')
-@example('.ytlast vlogbrothers')
-def ytlast(bot, trigger):
-    if not trigger.group(2):
-        return
-    uri = 'https://gdata.youtube.com/feeds/api/users/' + trigger.group(2) + '/uploads?max-results=1&alt=json&v=2'
-    video_info = ytget(bot, trigger, uri)
-
-    if video_info is 'err':
-        return
-
-    message = ('[Latest Video] Title: ' + video_info['title'] +
-               ' | Duration: ' + video_info['length'] +
-               ' | Uploaded: ' + video_info['uploaded'] +
-               ' | Views: ' + video_info['views'] +
-               ' | Likes: ' + video_info['likes'] +
-               ' | Dislikes: ' + video_info['dislikes'] +
-               ' | Link: ' + video_info['link'])
-
-    bot.say(HTMLParser().unescape(message))
+    bot.say(message)
