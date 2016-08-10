a1d73a06aa01535bc1fb4249a2327e87dc2bb9e0
Nikola Kovacevic <nikolak@outlook.com>
2016-08-10 14:51:21 +0200

    pypy sandbox response
    instead of no response show to the user that the feature is not available.

--- lpbot/modules/pypysandbox.py
+++ lpbot/modules/pypysandbox.py
@@ -1,58 +1,58 @@
-# #!/usr/bin/env python
-# # -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-# # PyPy code from: https://github.com/raylu/pbot/blob/master/commands.py
-
-
-
-# import os
-# from subprocess import Popen, PIPE, TimeoutExpired
-# import signal
-
-# from lpbot.module import commands, example
+# PyPy code from: https://github.com/raylu/pbot/blob/master/commands.py
 
 
-# PATH = os.environ['PATH']
+
+import os
+from subprocess import Popen, PIPE, TimeoutExpired
+import signal
+
+from lpbot.module import commands, example
 
 
-# def get_process_children(pid):
-#     p = Popen('ps --no-headers -o pid --ppid %d' % pid, shell=True,
-#               stdout=PIPE, stderr=PIPE)
-#     stdout, stderr = p.communicate()
-#     return [int(p) for p in stdout.split()]
+PATH = os.environ['PATH']
 
 
-# def python(code):
-#     pypy = Popen(['pypy-sandbox'],
-#                  stdin=PIPE, stdout=PIPE,
-#                  stderr=PIPE,
-#                  env={'PATH': PATH},
-#                  universal_newlines=True,
-#                  preexec_fn=os.setpgrp)
-#     try:
-#         stdout, stderr = pypy.communicate(code, timeout=5)
-#     except TimeoutExpired:
-#         os.killpg(pypy.pid, signal.SIGKILL)
-#         return "Timed out..."
-
-#     errlines = stderr.split('\n')
-#     if len(errlines) > 3:
-#         for i in range(1, len(errlines)):
-#             line = errlines[-i]  # iterate backwards
-#             if line:
-#                 return line[:250]
-#     else:
-#         for line in stdout.split('\n'):
-#             if line.startswith('>>>> '):
-#                 while line[:5] in ['>>>> ', '.... ']:
-#                     line = line[5:]
-#                 return line[:250]
+def get_process_children(pid):
+    p = Popen('ps --no-headers -o pid --ppid %d' % pid, shell=True,
+              stdout=PIPE, stderr=PIPE)
+    stdout, stderr = p.communicate()
+    return [int(p) for p in stdout.split()]
 
 
-# @commands('python', 'py')
-# @example('.py len([1,2,3])')
-# def bing(bot, trigger):
-#     pass
-#     code = trigger.group(2)
-#     result = python(code)
-#     bot.say(result)
+def python(code):
+    pypy = Popen(['pypy-sandbox'],
+                 stdin=PIPE, stdout=PIPE,
+                 stderr=PIPE,
+                 env={'PATH': PATH},
+                 universal_newlines=True,
+                 preexec_fn=os.setpgrp)
+    try:
+        stdout, stderr = pypy.communicate(code, timeout=5)
+    except TimeoutExpired:
+        os.killpg(pypy.pid, signal.SIGKILL)
+        return "Timed out..."
+
+    errlines = stderr.split('\n')
+    if len(errlines) > 3:
+        for i in range(1, len(errlines)):
+            line = errlines[-i]  # iterate backwards
+            if line:
+                return line[:250]
+    else:
+        for line in stdout.split('\n'):
+            if line.startswith('>>>> '):
+                while line[:5] in ['>>>> ', '.... ']:
+                    line = line[5:]
+                return line[:250]
+
+
+@commands('python', 'py')
+@example('.py len([1,2,3])')
+def pypy_sandbox(bot, trigger):
+    bot.say("This feature is currently not available.")
+    #code = trigger.group(2)
+    #result = python(code)
+    #bot.say(result)
