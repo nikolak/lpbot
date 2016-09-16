315b9117bcb8f0eff30aeeb57cc66ecdc098b6bf
Benjamin Eßer <esser@anvo-systems-dresden.com>
2016-09-16 13:12:10 +0200

    first working version with upvote, downvote, karma and reset_karma commands

--- lpbot/modules/karma.py
+++ lpbot/modules/karma.py
@@ -4,31 +4,67 @@
 
 from lpbot.module import commands
 
+def _set_karma(bot, trigger, change, reset=False):
+    """Helper function for increasing/decreasing/resetting user karma."""
+    channel = trigger.sender
+    user = trigger.group(2)
+    key = '{}_karma'.format(user)
+    
+    if reset:
+        bot.db.set_channel_value(channel, key, 0)
+        return
+    
+    karma = bot.db.get_channel_value(channel, key)
+    karma = int(karma) if karma else 0
+    if karma or change > 0:
+        bot.db.set_channel_value(channel, key, karma + change)
+        return karma + change
+    else:
+        return -1
+
 @commands('upvote')
 def upvote(bot, trigger):
-    """Give a user positive karma for being helpful."""
-    if not trigger.group(2):
+    """Give a user karma for being helpful."""
+    user = trigger.group(2)
+    if not user:
         bot.say(".upvote <nick> - Give <nick> karma for being helpful.")
         return
-    bot.say('Trying to get karma value for user %s in channel %s...' % (trigger.group(2), trigger.sender))
-    karma = bot.db.get_channel_value(trigger.sender, trigger.group(2))
-    bot.say('Karma for user %s is %s.' % (trigger.group(2), karma))
-    bot.db.set_channel_value(trigger.sender, '%s_karma' % trigger.group(2), karma+1)
-    bot.say('Added 1 karma to %s, for a total of %s.' % (trigger.group(2), karma+1))
-	
+    karma = _set_karma(bot, trigger, 1)
+    bot.say('{}\'s karma increased by 1, {} now has a total karma of {}.'.format(user,
+                                                                                 user,
+                                                                                 karma))
+
 @commands('downvote')
 def downvote(bot, trigger):
     """Substract karma (if karma is positive) from a user for not being helpful."""
-    #TODO check if karma > 0
-    if not trigger.group(2):
+    user = trigger.group(2)
+    if not user:
         bot.say(".downvote <nick> - Take karma away from <nick>.")
         return
+    karma = _set_karma(bot, trigger, -1)
+    if karma == -1:
+        bot.say("{} currently has no karma.".format(user))
+        return
+    else:
+        bot.say("It seems {} wasn\'t very helpful. Karma decreased to {}.".format(user,
+                                                                                  karma))
 
 @commands('karma')
 def karma(bot, trigger):
     """Report the karma for a user in this channel."""
-    if not trigger.group(2):
+    user = trigger.group(2)
+    if not user:
         bot.say(".karma <nick> - Report karma for <nick>.")
         return
-    karma = bot.db.get_channel_value(trigger.sender, '%s_karma' % trigger.group(2))
-    bot.say('%s has %s karma.' % (trigger.group(2), karma))
+    karma = bot.db.get_channel_value(trigger.sender, '{}_karma'.format(user)) or 0
+    bot.say('{} has {} karma in {}.'.format(user, karma, trigger.sender))
+    
+@commands('reset_karma')
+def reset_karma(bot, trigger):
+    "Reset user karma to 0."
+    user = trigger.group(2)
+    if not user:
+        bot.say(".reset_karma <nick> - Set <nick>\'s karma to 0.")
+        return
+    karma = _set_karma(bot, trigger, 0, reset=True)
+    bot.say("{}\'s karma reset to 0.".format(user))
